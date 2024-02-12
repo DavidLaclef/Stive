c7ac15d0fce62739777b56a5c4fe87191b63a979
DavidLaclef <daev.cognita@gmail.com>
2024-02-12 22:15:05 +0100

    Ajout décorateurs

--- Models/Dao/Client.cs
+++ Models/Dao/Client.cs
@@ -5,20 +5,20 @@ namespace Models.Dao;
 public class Client : Personne
 {
     [Required]
-    [StringLength(10)]
+    [StringLength(10, MinimumLength = 10)]
     public string CodeClient { get; set; } = string.Empty;
 
-    [StringLength(80)]
+    [StringLength(80, MinimumLength = 3)]
     public string Entreprise { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(10)]
+    [StringLength(10, MinimumLength = 10)]
     public string NumeroTelephone { get; set; } = string.Empty;
 
-    [StringLength(80)]
+    [StringLength(80, MinimumLength = 3)]
     public string NomLivraison { get; set; } = string.Empty;
 
-    [StringLength(80)]
+    [StringLength(80, MinimumLength = 3)]
     public string PrenomLivraison { get; set; } = string.Empty;
 
     [Required]
@@ -29,11 +29,11 @@ public class Client : Personne
     public string AdressePostale { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(5)]
+    [StringLength(5, MinimumLength = 5)]
     public string CodePostal { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(80)]
+    [StringLength(80, MinimumLength = 3)]
     public string Ville { get; set; } = string.Empty;
 
     [StringLength(150)]
