6ec145a6651b62423920861accab01db46dcdcc4
Daev <daev.cognita@gmail.com>
2024-02-22 12:15:09 +0100

    GET produits sous le seuil de réapprovisionnement

--- API/Controllers/ProduitsController.cs
+++ API/Controllers/ProduitsController.cs
@@ -29,6 +29,19 @@ public class ProduitsController : ControllerBase
             .ToListAsync();
     }
 
+    // GET: api/Produits
+    [HttpGet]
+    [Route("SousSeuilReapprovisionnement")]
+    public async Task<ActionResult<IEnumerable<ProduitLightDto>>> GetProduitSousSeuilReapprovisionnement()
+    {
+        return await _context.Produit
+            .Include(p => p.Chateau)
+            .Include(p => p.Familles)
+            .Where(p => p.Quantite <= p.SeuilReapprovisionnement)
+            .Select(p => p.ToLightDto())
+            .ToListAsync();
+    }
+
     // GET: api/Produits/5
     [HttpGet("{id}")]
     public async Task<ActionResult<ProduitDto>> GetProduit(int id)
