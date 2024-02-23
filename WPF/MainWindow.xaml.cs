107639fbbdc2ae4ed6864cc3988cc01cf8b1f9c2
Daev <daev.cognita@gmail.com>
2024-02-23 12:56:36 +0100

    Identation

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using WPF.Services;
 using WPF.ViewModels;
 using WPF.Views.Accueil;
 using WPF.Views.Client;
@@ -8,100 +7,99 @@ using WPF.Views.Fournisseur;
 using WPF.Views.Stock;
 using WPF.Views.Utilisateur;
 
-namespace WPF
+namespace WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            // Gestion de l'évènement dans le code directement
-            BtnConnexion.Click += BtnConnexion_Click;
-            BtnConnexion_Click(BtnConnexion, null);
+        // Gestion de l'évènement dans le code directement
+        BtnConnexion.Click += BtnConnexion_Click;
+        BtnConnexion_Click(BtnConnexion, null);
 
-            //this.DataContext = MainViewModel.Instance; // Sur cette branche, MainViewModel est créé (je l'ai créé)
+        //this.DataContext = MainViewModel.Instance; // Sur cette branche, MainViewModel est créé (je l'ai créé)
 
-            //Task.Run(async () => await HttpClientService.Login("jeff.harbeng@stive.com", "Jeff1."));
+        //Task.Run(async () => await HttpClientService.Login("jeff.harbeng@stive.com", "Jeff1."));
 
-        }
+    }
 
-        private void BtnAccueil_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new UcAccueil();
-            uc.DataContext = uc;
-            mainCC.Content = uc;
+    private void BtnAccueil_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new UcAccueil();
+        uc.DataContext = uc;
+        mainCC.Content = uc;
 
-        }
+    }
 
-        private void BtnStocks_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new UcStock();
-            uc.DataContext = uc;
-            mainCC.Content = uc;
+    private void BtnStocks_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new UcStock();
+        uc.DataContext = uc;
+        mainCC.Content = uc;
 
-        }
+    }
 
-        private void BtnClient_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new UcClient();
-            uc.DataContext = new ClientsViewModel();
-            mainCC.Content = uc;
+    private void BtnClient_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new UcClient();
+        uc.DataContext = new ClientsViewModel();
+        mainCC.Content = uc;
 
-        }
+    }
 
-        private void BtnFournisseur_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new UcFournisseur();
-            uc.DataContext = new FournisseursViewModel();
-            mainCC.Content = uc;
-        }
+    private void BtnFournisseur_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new UcFournisseur();
+        uc.DataContext = new FournisseursViewModel();
+        mainCC.Content = uc;
+    }
 
-        private void BtnUtilisateur_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new UcUtilisateur();
-            uc.DataContext = new UtilisateursViewModel();
-            mainCC.Content = uc;
-        }
+    private void BtnUtilisateur_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new UcUtilisateur();
+        uc.DataContext = new UtilisateursViewModel();
+        mainCC.Content = uc;
+    }
 
-        private void BtnConnexion_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new ucConnexionUtilisateur();
-            uc.ConnexionChange += Uc_ConnexionChange;
-            uc.DataContext = new UtilisateursAuthViewModel();
-            mainCC.Content = uc;
-        }
+    private void BtnConnexion_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new ucConnexionUtilisateur();
+        uc.ConnexionChange += Uc_ConnexionChange;
+        uc.DataContext = new UtilisateursAuthViewModel();
+        mainCC.Content = uc;
+    }
 
-        private void Uc_ConnexionChange(object? sender, EventArgs e)
-        {
-            BtnAccueil.Visibility = Visibility.Visible;
-            BtnStocks.Visibility = Visibility.Visible;
-            BtnClient.Visibility = Visibility.Visible;
-            BtnFournisseur.Visibility = Visibility.Visible;
-            BtnUtilisateur.Visibility = Visibility.Visible;
+    private void Uc_ConnexionChange(object? sender, EventArgs e)
+    {
+        BtnAccueil.Visibility = Visibility.Visible;
+        BtnStocks.Visibility = Visibility.Visible;
+        BtnClient.Visibility = Visibility.Visible;
+        BtnFournisseur.Visibility = Visibility.Visible;
+        BtnUtilisateur.Visibility = Visibility.Visible;
 
-            // Boutons relatifs au compte utilisateurs
-            BtnConnexion.Visibility = Visibility.Collapsed;
-            BtnNouvelleUtilisateur.Visibility = Visibility.Visible;
-            // Redirection vers L'acceuil
-            BtnAccueil_Click(BtnAccueil, null);
-        }
+        // Boutons relatifs au compte utilisateurs
+        BtnConnexion.Visibility = Visibility.Collapsed;
+        BtnNouvelleUtilisateur.Visibility = Visibility.Visible;
+        // Redirection vers L'acceuil
+        BtnAccueil_Click(BtnAccueil, null);
+    }
 
-        private void BtnNouvelleUtilisateur_Click(object sender, RoutedEventArgs e)
-        {
-            mainCC.Content = null;
-            var uc = new ucEnregistrementUtilisateur();
-            uc.DataContext = new UtilisateursAuthViewModel();
-            mainCC.Content = uc;
-        }
+    private void BtnNouvelleUtilisateur_Click(object sender, RoutedEventArgs e)
+    {
+        mainCC.Content = null;
+        var uc = new ucEnregistrementUtilisateur();
+        uc.DataContext = new UtilisateursAuthViewModel();
+        mainCC.Content = uc;
     }
 }
